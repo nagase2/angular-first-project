b553693d6dc538de48394652cbd6184c56c5913d
yoshi-PC\yoshi <nagase2@gmail.com>
2018-06-07 00:50:45 +0900

    https://angular.io/tutorial/toh-pt4途中

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,11 +12,12 @@ export class HeroesComponent implements OnInit {
   // hero = 'Windstorm!!!';
   hero: Hero = {
     id: 1,
-    name: 'nagase'
+    name: 'nagase',
+    age: 40
   };
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
   }
--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,14 +1,10 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Mr. Nice', age: 2},
+  { id: 12, name: 'Narco', age: 2},
+  { id: 13, name: 'Bombasto', age: 2},
+  { id: 14, name: 'Celeritas', age: 2 },
+  { id: 15, name: 'Magneta', age: 2 }
 ];
+
