3aa5a832ce2826ea509edf0c56acdd5c487065da
yoshi-PC\yoshi <nagase2@gmail.com>
2018-09-27 00:00:25 +0900

    part1 sources added

---
+++ part1/src/app/authors/authors.component.ts
@@ -0,0 +1,23 @@
+import { ListService } from './../list.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent implements OnInit {
+  authors: string[];
+
+  constructor(listService: ListService) {
+    this.authors = listService.authors;
+  }
+
+  public countAuthor(): number {
+    return this.authors.length;
+  }
+
+  ngOnInit() {
+  }
+
+}
