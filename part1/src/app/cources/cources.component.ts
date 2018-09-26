95b0a41e2a9f4c6dc0ca63c6149f1142f66adc63
yoshi-PC\yoshi <nagase2@gmail.com>
2018-09-27 00:56:19 +0900

    enterkeyを取得する。

--- part1/src/app/cources/cources.component.ts
+++ part1/src/app/cources/cources.component.ts
@@ -2,15 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cources',
-  template: `
-    <button class="btn btn-primary" >save</button>
-  `,
+  template: `<hr>
+    <button class="btn btn-primary" [class.active]="isActive">save</button>
+    <br>
+    <button
+    (click)="onSave($event)"
+    class="btn btn-primary"  [class.active]="isActive"
+     [style.backgroundColor]="isActive?'blue':'white'">sssxxxxx</button>
+     <br>
+     <input (keyup.enter)="onKeyUp()"/>
+     `,
   styleUrls: ['./cources.component.css']
 })
 export class CourcesComponent implements OnInit {
-
+  isActive = true;
   constructor() { }
+  onSave($event) {
 
+    console.log($event);
+    console.log('x座標:' + $event.clientX);
+  }
+  onKeyUp() {
+    console.log("enter was pressed.");
+  }
   ngOnInit() {
   }
 
