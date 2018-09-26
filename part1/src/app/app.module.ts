2ee85392ec3f55ffedffaa025890deac55dcce25
yoshi-PC\yoshi <nagase2@gmail.com>
2018-09-27 00:22:00 +0900

    bootstrap追加

--- part1/src/app/app.module.ts
+++ part1/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { CourcesComponent } from './cources/cources.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    CourcesComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent, AuthorsComponent]
+  bootstrap: [AppComponent, AuthorsComponent, CourcesComponent]
 })
 export class AppModule { }
---
+++ part1/src/app/cources/cources.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cources',
+  template: `
+    <button class="btn btn-primary" >save</button>
+  `,
+  styleUrls: ['./cources.component.css']
+})
+export class CourcesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
