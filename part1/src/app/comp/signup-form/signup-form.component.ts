d78fd1d6066668c3499d835b7297c95bb603e03d
Naggi <nagase2@gmail.com>
2018-10-06 22:34:21 +0900

    バージョン更新

--- part1/src/app/comp/signup-form/signup-form.component.ts
+++ part1/src/app/comp/signup-form/signup-form.component.ts
@@ -1,5 +1,5 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UsernameValidators } from './username.validators';
 @Component({
@@ -7,21 +7,46 @@ import { UsernameValidators } from './username.validators';
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css']
 })
-export class SignupFormComponent {
-  myform = new FormGroup({
-    username: new FormControl('',
-      [Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(10),
-      UsernameValidators.cannotContainSpace]),
+export class SignupFormComponent implements OnInit {
 
-    password: new FormControl('', Validators.required)
+  myform: FormGroup;
+
+  ngOnInit(): void {
+    //throw new Error("Method not implemented.");
+
+  this.myform = new FormGroup({
+    username: new FormControl('',
+      [
+        Validators.minLength(3),
+        Validators.maxLength(10),
+        UsernameValidators.cannotContainSpace,
+        UsernameValidators.cannotContainAAA,
+        //これを有効にするとうまく動かないので、一旦コメントアウトする。
+       // UsernameValidators.shouldBeUnique,
+        Validators.required
+      ]),
+
+    password: new FormControl('',
+      [Validators.required])
   });
+  }
+
 
   /**
    * get username from myform property
    */
   get username() {
     return this.myform.get('username');
+  }
+
+  get password() {
+    return this.myform.get('password');
+  }
+
+
+  login() {
+    this.myform.setErrors({
+      invalidLogin: true
+    });
   }
 }
--- part1/src/app/comp/signup-form/username.validators.ts
+++ part1/src/app/comp/signup-form/username.validators.ts
@@ -1,4 +1,5 @@
-import { AbstractControl, ValidationErrors } from "@angular/forms";
+import { AbstractControl, ValidationErrors, AsyncValidatorFn, AsyncValidator } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 /**
  * this is custom valdator class..
@@ -11,5 +12,38 @@ export class UsernameValidators {
             return { cannotContainSpace: true };
         }
         return null;
+
     }
+    static cannotContainAAA(control: AbstractControl): ValidationErrors | null {
+        if ((control.value as string).indexOf('AAA') >= 0) {
+            return { cannotContainAAA: true };
+        }
+        return null;
+    }
+
+    /**
+     * 非同期バリデーションサンプル
+     * @param control 
+     */
+    static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === 'naga') {
+                    resolve({ shouldBeUnique: true });
+                } else {
+                    resolve(null);
+                }
+            }, 2000);
+        });
+    }
+}
+
+export class UniqueUserValidator implements AsyncValidator {
+    validate(c: AbstractControl): Promise<ValidationErrors> | Observable<ValidationErrors> {
+        throw new Error("Method not implemented.");
+    }    registerOnValidatorChange?(fn: () => void): void {
+        throw new Error("Method not implemented.");
+    }
+
+
 }
