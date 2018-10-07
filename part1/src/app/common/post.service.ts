e050aea0ea33306d4ac10496c28a6ae5ce053266
Naggi <nagase2@gmail.com>
2018-10-08 00:33:10 +0900

    update

--- part1/src/app/common/post.service.ts
+++ part1/src/app/common/post.service.ts
@@ -1,9 +1,27 @@
+import { Observable } from 'rxjs';
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+//import 'rxjs/add/operator/catch';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  url = 'https://jsonplaceholder.typicode.com/posts';
+  //posts: any[];
+
+  constructor(private http: Http) { }
+
+  getPosts() {
+    return this.http.get(this.url);
+  }
+
+  deletePost(post) {
+    return this.http.delete(this.url + '/' + post.id);
+  }
+
+  updatePost(post) {
+    return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }));
+  }
 }
--- part1/src/app/comp/post/post.component.ts
+++ part1/src/app/comp/post/post.component.ts
@@ -1,3 +1,4 @@
+import { PostService } from './../../common/post.service';
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
@@ -7,45 +8,63 @@ import { Http } from '@angular/http';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements onInit {
-  posts: any[];
- // http: Http;
-  url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http: Http) {
-    //this.http = http;
+  posts: any[];
+  constructor(private postService: PostService) {
+
   }
 
   ngOnInit(): void {
-    this.http.get(this.url)
+    this.postService.getPosts()
       .subscribe(response => {
         this.posts = response.json();
         //console.log(response.json());
+      }, error => {
+        alert('an unexpected error happend');
+        console.log(error);
       });
-
   }
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
-    this.http.post(this.url, JSON.stringify(post)).subscribe(x => {
+    this.postService.getPosts().subscribe(x => {
       // リクエストの戻り値をpostに設定
       post['id'] = x.json().id;
       // postsの最初に登録した値を追加する。
       this.posts.splice(0, 0, post);
       // console.log(x.json());
+    }, (error: Response) => {
+      if (error.status === 400) {
+        // this.form.setErrors(error.json));
+
+      } else {
+        alert('an unexpected error occurred.');
+        console.log(error);
+      }
+
     });
   }
 
+
   updatePost(post) {
-    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+    this.postService.updatePost(post)
+      // this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
       .subscribe(x => {
         console.log(x.json());
       });
   }
 
   deletePost(post) {
-    this.http.delete(this.url + '/' + post.id).subscribe(response => {
+    this.postService.deletePost(post).subscribe(response => {
       let index = this.posts.indexOf(post);
       this.posts.splice(index, 1);
+    }, (error: Response) => {
+      if (error.status === 404) {
+        alert('this post has already been deteled.');
+      } else {
+        alert('An unexpected error occurred.');
+      }
+      console.log(error)
     });
   }
 
