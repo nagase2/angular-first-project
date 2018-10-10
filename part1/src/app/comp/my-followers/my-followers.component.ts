0f7cb6fb6fd462503197d48bb2aa79d5c2b50f78
yoshimasa_nagse <yoshimasa_nagase@ss.toyota-tsusho.com>
2018-10-10 12:54:57 +0900

    queryparamMapのサンプルを実装

--- part1/src/app/comp/my-followers/my-followers.component.ts
+++ part1/src/app/comp/my-followers/my-followers.component.ts
@@ -2,7 +2,7 @@ import { MyFollowerService } from '../../common/my-follower.service';
 
 
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-my-followers',
@@ -11,13 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyFollowersComponent implements OnInit {
 
-  constructor(private service: MyFollowerService) { }
+  constructor(private service: MyFollowerService,
+              private route: ActivatedRoute
+            ) { }
   followers: any[];
 
   ngOnInit() {
+    //何度も同ページを移動する場合はobservableを利用する。
+    this.route.queryParamMap.subscribe(param =>{
+      console.log(param);
+    });
+    //他で値を利用しない場合はこちら
+    console.log("this is id "+this.route.snapshot.queryParamMap.get('page'));
+
     this.service.getFollowers().subscribe(response => {
       this.followers = response.json();
-      console.log(this.followers);
+      //console.log(this.followers);
     });
   }
 
