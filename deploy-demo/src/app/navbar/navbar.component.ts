ffa77b7b562cac5f1f7b07e5daf87c2043589049
Naggi <nagase2@gmail.com>
2018-10-14 15:22:31 +0900

    deploy-demo added

---
+++ deploy-demo/src/app/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { environment } from './../../environments/environment';
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  backgroundColor = environment.navBarBackgroundColor;
+  envName = environment.envName;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
