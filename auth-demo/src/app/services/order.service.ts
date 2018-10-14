36e9322b9b650d065ae757ca282f5304e516889d
Naggi <nagase2@gmail.com>
2018-10-14 10:06:27 +0900

    update

--- auth-demo/src/app/services/order.service.ts
+++ auth-demo/src/app/services/order.service.ts
@@ -1,18 +1,23 @@
-import { Http } from '@angular/http';
+import { AuthHttp } from 'angular2-jwt';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class OrderService {
 
- 
-  constructor(private http: Http) {
+
+  constructor( private http: Http) {
   }
 
   getOrders() {
     let headers = new Headers()
-  
-    return this.http.get('/api/orders')
+    let token = localStorage.getItem('token')
+    headers.append('Authorization', 'Bearer ' + token)
+    let options = new RequestOptions({ headers: headers})
+
+    return this.http.get('/api/orders',options)
       .map(response => response.json());
+    
   }
 }
