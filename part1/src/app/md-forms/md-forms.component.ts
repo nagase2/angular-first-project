425e49d3a2d6e5fbd4f80bb90408560658473425
Naggi <nagase2@gmail.com>
2018-10-18 00:36:24 +0900

    update

--- part1/src/app/md-forms/md-forms.component.ts
+++ part1/src/app/md-forms/md-forms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatCheckbox } from '@angular/material/checkbox';
 
 @Component({
@@ -6,17 +6,22 @@ import { MatCheckbox } from '@angular/material/checkbox';
   templateUrl: './md-forms.component.html',
   styleUrls: ['./md-forms.component.css']
 })
-export class MdFormsComponent implements OnInit {
+export class MdFormsComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+    console.log("on destoroy")
+  }
   checked = false;
   indeterminate = true;
 
   constructor() { }
 
   ngOnInit() {
+    console.log("on init" + this.indeterminate);
   }
 
   onchange(ind: MatCheckbox) {
     console.log(ind.checked);
+    this.indeterminate = ind.checked;
   }
 
 }
