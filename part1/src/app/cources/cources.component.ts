e7888b8a42fff8cefc755212fc4a48022af8ff24
Yoshimasa Nagase <yoshimasa_nagase@tsyscom.co.jp>
2018-10-26 16:35:02 +0900

    sidemenuをアップデート（途中）

--- part1/src/app/cources/cources.component.ts
+++ part1/src/app/cources/cources.component.ts
@@ -4,9 +4,10 @@ import { switchMap } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 //import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, CollectionReference } from 'angularfire2/firestore';
 import { FirebaseAuth, FirebaseApp } from 'angularfire2';
 import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cources',
@@ -16,14 +17,29 @@ import { FormControl } from '@angular/forms';
 export class CourcesComponent implements OnInit {
   isActive = true;
   public items: Observable<any[]>;
-
+  // items2$: Observable<Item[]>;
+  //item : CollectionReference<item>;
+ 
 
   constructor(private fireStore: AngularFirestore, fireAuth: FirebaseApp) {
     this.items = fireStore.collection('/items').valueChanges();
 
-    // this.items = fireStore.collection('items', (ref) => {
+    var itemRef = fireStore.collection('/items')
+    // var query = itemRef.
+
+    // fireStore.collection('items', (ref) => {
     //   ref.where('name', '==', 'nagase')
     // })
+//     var citiesRef = db.collection('cities');
+// var query = citiesRef.where('capital', '==', true).get()
+//   .then(snapshot => {
+//     snapshot.forEach(doc => {
+//       console.log(doc.id, '=>', doc.data());
+//     });
+//   })
+//   .catch(err => {
+//     console.log('Error getting documents', err);
+//   });
 
     //afs.collection('items', ref => ref.where('size', '==', 'large'))
 
@@ -53,5 +69,11 @@ export class CourcesComponent implements OnInit {
   ngOnInit() {
 
   }
-
 }
+
+export interface Item{
+  id:string
+  name:string
+  value:string
+  age:number
+} 
