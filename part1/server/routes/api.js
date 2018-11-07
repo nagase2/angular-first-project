5baac2c10306119a0cdb02dd1ec654db290b22ec
Naggi <nagase2@gmail.com>
2018-11-08 00:41:49 +0900

    update

--- part1/server/routes/api.js
+++ part1/server/routes/api.js
@@ -1,19 +1,40 @@
 const express = require('express')
 const router = express.Router()
-
+const User = require('../model/user')
 const mongoose = require('mongoose')
 const db = "mongodb://admin:admin123@ds151853.mlab.com:51853/eventdb"
 
 mongoose.connect(db, err => {
     if (err) {
-        console.error('Error'+ err)
+        console.error('Error' + err)
     } else {
         console.log('Connected to mongodb')
     }
 })
 
+router.get('/test', (req, res) => {
+    console.log(req.query)
+    res.send('OK test')
+})
+
 router.get('/', (req, res) => {
     res.send('From API route')
+})
+
+router.post('/register', (req, res) => {
+
+    let userData = req.body
+    console.log(req)
+    let user = new User(userData)
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error)
+        }
+        else{
+            console.log("P:" + registeredUser)
+            res.status(200).send(registeredUser)
+        }
+    })
 })
 
 module.exports = router;
